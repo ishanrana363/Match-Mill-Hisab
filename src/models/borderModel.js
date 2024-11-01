f0999cb2c82ddf3ba3d6a82e32c91d3efe9a00cf
ishanrana363 <ishanrana363@gmail.com>
2024-11-01 18:20:21 +0600

    border model created

---
+++ src/models/borderModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const {Schema,model} = mongoose;
+
+const borderSchema = new Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    phone : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    address : {
+        type: String,
+        required: true
+    },
+    father_name : {
+        type: String,
+        required: true
+    },
+    mother_name : {
+        type: String,
+        required: true
+    },
+    dob : {
+        type : String,
+        required: true
+    },
+    father_phone_number : {
+        type: String,
+        required: true
+    },
+    institute_name : {
+        type: String,
+        required: true
+    }
+},{timestamps:true,versionKey:false});
+
+const borderModel = model("border",borderSchema);
+
+module.exports = borderModel;
