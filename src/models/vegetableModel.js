6bfa5e89354ac07232f549631616d7115b25cdd4
ishanrana363 <ishanrana363@gmail.com>
2024-11-02 19:43:41 +0600

    vegetable model created

---
+++ src/models/vegetableModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const {Schema,model} = mongoose;
+
+const vegetableSchema = new Schema({
+    borderId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'Border', // Assuming there is a 'Border' model to populate
+    },
+    mill : {
+        type : String,
+    },
+    date : {
+        type : Date,
+    }
+},{timestamps: true,versionKey:false});
+
+const vegetableModel = model('Vegetable', vegetableSchema);
+
+module.exports = vegetableModel;
