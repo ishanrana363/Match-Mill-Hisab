692951dbad09ea795b4765df4f9e05fd31bfd02f
ishanrana363 <ishanrana363@gmail.com>
2024-11-02 14:06:06 +0600

    api modify

--- src/models/dailyRiceModel.js
+++ src/models/dailyRiceModel.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 
-const {Schema,model} = mongoose;
+const { Schema, model } = mongoose;
 
-const dailyRiceSchema = new Schema({
-    borderId : {
-        type : mongoose.Schema.Types.ObjectId,
+const dailyRiceSchema = new Schema(
+    {
+        borderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Border', // Assuming there is a 'Border' model to populate
+        },
+        pot: {
+            type: String, 
+            default: 0,
+        },
+        entry_date: {
+            type: Date,
+            
+        },
     },
-    pot : {
-        type : String,
-        default : 0
-    },
-    date : {
-        type : Date,
-    }
-    
+    { timestamps: true, versionKey: false }
+);
 
-},{timestamps: true,versionKey:false});
-
-const dailyRiceModel = model("daily-rice", dailyRiceSchema);
+const dailyRiceModel = model('DailyRice', dailyRiceSchema); // Changed to singular form for model naming convention
 
 module.exports = dailyRiceModel;
