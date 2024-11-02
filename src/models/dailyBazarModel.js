91a6647df43060c893687a9a838b4206cc6f65f9
ishanrana363 <ishanrana363@gmail.com>
2024-11-02 21:56:46 +0600

    daily bazar model created

---
+++ src/models/dailyBazarModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const {Schema,model} = mongoose;
+
+const dailyBazarSchema = new Schema({
+    borderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Border', // Assuming there is a 'Border' model to populate
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    itemName : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : String,
+        required : true
+    }
+},{ timestamps: true,versionKey:false});
+
+const dailyBazarModel = model('daily-bazar', dailyBazarSchema);
+
+module.exports = dailyBazarModel;
