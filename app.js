8db34b35e269ad710b62e8620f670312a681c4ee
ishanrana363 <ishanrana363@gmail.com>
2024-11-04 23:18:08 +0600

    some added

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.use(mongoSanitize())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('trust proxy', 'loopback'); // Only trust localhost proxy
-app.set('trust proxy', 1); // Trust the first proxy
+// app.set('trust proxy', 'loopback'); // Only trust localhost proxy
+// app.set('trust proxy', 1); // Trust the first proxy
 // Database Connect 
 
 app.get("/",(req, res) => {
